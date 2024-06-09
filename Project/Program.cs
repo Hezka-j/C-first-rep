9b68c8c59ce877354c4e2e4530449759afdfc6cb
Hezka-j <145163687+Hezka-j@users.noreply.github.com>
2024-06-09 12:38:26 +0300

    Add files via upload

---
+++ Project/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            
+            Console.CursorVisible = true;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Name: Elina");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Last name: Varenyk");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Age: 17");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Country: Ukraine");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Pets: Dog Lucky");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Education: School, a little bit IT");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Number: 063-607-1741");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Languages: ukrainian, a little-bit english, more than a little bit C++, a little bit german, a little bit Korean");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Для теста:Вода - 110 мл");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Яйцо - 1 шт.\nМука - 300 г (2 стакана ёмкостью 250 мл) + для работы с тестом");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Соль - 1 щепотка\n*\nДля начинки:");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Картофель (очищенный) - 500 г\nЛук репчатый - 1 шт.");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Соль\nПерец черный молотый");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Масло растительное для жарки - 30 мл\n*\nДля подачи (по желанию):\nСметана");
+        
+        }
+    }
+}
